7e2f698592e2a95ab9e5cdb51c857bccbfc47d77
Denis Carriere <carriere.denis@gmail.com>
2019-05-08 12:35:40 -0400

    Update readme

--- examples/test-oracle.ts
+++ examples/test-oracle.ts
@@ -2,16 +2,27 @@ import { transact } from "../src/utils";
 import { Action } from "../src/interfaces";
 
 (async () => {
+    // Optional paramaters
+    const from = "EOS";
+    const to = "USD";
+
+    // DSP URI request
+    const endpoint = `api.bancor.network/0.1/currencies/rate?toCurrencyCode=${from}&fromCurrencyCodes=${to}`;
+    const uri = `https+json://data.${to}/${endpoint}`
+
+    // EOSIO action
     const action: Action = {
-        account: "oraclenation",
-        name: "testrnd",
+        account: "oraclenation", // [oracle-dapp-service/oracleconsumer] smart contract
+        name: "testrnd", // test oracle action
         authorization: [
             {actor: "oraclenation", permission: "ops"}
         ],
         data: {
-            uri: Buffer.from('https+json://data.USD/api.bancor.network/0.1/currencies/rate?toCurrencyCode=EOS&fromCurrencyCodes=USD', 'utf8'),
+            // uri is converted into bytes (Buffer)
+            uri: Buffer.from(uri, 'utf8'),
         }
     }
+    // Push transaction
     await transact([action]).catch(e => console.error(e));
 
 })().catch(e => console.error(e));
